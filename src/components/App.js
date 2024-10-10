73570d15801c84bdd20d40a9b404f0e0f00be899
Kelly Netterville <36lima@gmail.com>
2024-10-10 17:30:17 -0500

    Initial react setup and base config.

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+// Will use React functional components and hooks to build the front end of the dapp.
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
